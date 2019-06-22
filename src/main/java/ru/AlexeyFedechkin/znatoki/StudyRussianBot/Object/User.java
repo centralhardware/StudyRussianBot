90edee26219a32c8310305a6607d9ef509b5a601
Alexey Fedechkin <aleksey-fedechkin@rambler.ru>
2019-06-22 12:56:02 +0700

    add copyright

--- src/main/java/ru/AlexeyFedechkin/znatoki/StudyRussianBot/Object/User.java
+++ src/main/java/ru/AlexeyFedechkin/znatoki/StudyRussianBot/Object/User.java
@@ -1,3 +1,9 @@
+/*
+ * Author: Fedechkin Alexey Borisovich
+ * last modified: 22.06.19 12:55
+ * Copyright (c) 2019
+ */
+
 package ru.AlexeyFedechkin.znatoki.StudyRussianBot.Object;
 
 import ru.AlexeyFedechkin.znatoki.StudyRussianBot.Object.Enums.UserStatus;
