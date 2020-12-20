0b074ccaf2aeaebc598ac0b0e4f4dfd5ed2f8953
Alexey Fedechkin <alex@centralhardware.ru>
2020-12-20 05:06:27 +0000

    add automatic docker image build

--- .space.kts
+++ .space.kts
@@ -17,3 +17,34 @@ job("execute test") {
         }
     }
 }
+
+job("Build and push Docker") {
+    container("maven:3.6.3-openjdk-15") {
+        resources {
+            cpu = 1.cpu
+            memory = 1024.mb
+        }
+        shellScript {
+            content = """
+                mvn package
+            """
+        }
+        kotlinScript { api -> 
+			api.space().chats.channels.messages.sendTextMessage(
+    			channelId = "3MVaBr2rDLSR",
+    			text = "build image registry.centralhardware.synology.me/studyrussianbot:7.${api.executionNumber()} ")
+    	}
+    }
+
+    docker {     
+		build {
+            context = "./"
+            labels["build"] = "centralhardware.jetbrains.space"
+            file = "dockerfile"
+        }
+        
+        push("registry.centralhardware.synology.me/studyrussianbot") {
+            tag = "7.\$JB_SPACE_EXECUTION_NUMBER"
+        }
+	}
+}
