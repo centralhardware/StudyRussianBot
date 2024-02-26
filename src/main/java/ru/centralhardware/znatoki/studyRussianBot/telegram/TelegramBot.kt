68287d33825feb4e6ad1f62ba6dedefc25870193
Alexey Fedechkin <alex@centralhardware.me>
2024-02-26 22:21:49 +0100

    refactor

--- src/main/java/ru/centralhardware/znatoki/studyRussianBot/telegram/TelegramBot.kt
+++ src/main/java/ru/centralhardware/znatoki/studyRussianBot/telegram/TelegramBot.kt
@@ -29,7 +29,7 @@ class TelegramBot : TelegramLongPollingBot(Config.token) {
         inlineKeyboard = InlineKeyboard(sender)
     }
 
-    companion object{
+    companion object {
         private val logger = LoggerFactory.getLogger(TelegramBot::class.java)
 
         /**
@@ -55,11 +55,17 @@ class TelegramBot : TelegramLongPollingBot(Config.token) {
      * @param update received message
      */
     override fun onUpdateReceived(update: Update) {
-        runBlocking { process(update) }
+        runBlocking {
+            try {
+                process(update)
+            } catch (t: Throwable) {
+                logger.warn("", t)
+            }
+        }
     }
 
-    private suspend fun process(update: Update){
-        clickhouse.log(update,"StudyRussianBot")
+    private suspend fun process(update: Update) {
+        clickhouse.log(update, "StudyRussianBot")
 
         if (telegramParser == null) telegramParser = TelegramParser(sender)
 
@@ -70,26 +76,34 @@ class TelegramBot : TelegramLongPollingBot(Config.token) {
         }
         when {
             update.hasCallbackQuery() -> {
-                logger.info("receive callback \"" + update.callbackQuery.data + "\" " +
-                        update.callbackQuery.from.firstName + "\" " +
-                        update.callbackQuery.from.lastName + "\" " +
-                        update.callbackQuery.from.userName + "\"")
+                logger.info(
+                    "receive callback \"" + update.callbackQuery.data + "\" " +
+                            update.callbackQuery.from.firstName + "\" " +
+                            update.callbackQuery.from.lastName + "\" " +
+                            update.callbackQuery.from.userName + "\""
+                )
                 if (!telegramParser!!.users.containsKey(update.callbackQuery.message.chatId)) {
-                    telegramParser!!.users[update.callbackQuery.message.chatId] = User(update.callbackQuery.message.chatId)
+                    telegramParser!!.users[update.callbackQuery.message.chatId] =
+                        User(update.callbackQuery.message.chatId)
                 }
                 telegramParser!!.parsCallback(update)
             }
+
             update.message.hasText() -> {
-                logger.info("receive message \"" + update.message.text +
-                        "\" from \"" + update.message.from.firstName + "\" " +
-                        update.message.from.lastName + "\" " +
-                        update.message.from.userName + "\" " +
-                        update.message.from.id + "\"")
+                logger.info(
+                    "receive message \"" + update.message.text +
+                            "\" from \"" + update.message.from.firstName + "\" " +
+                            update.message.from.lastName + "\" " +
+                            update.message.from.userName + "\" " +
+                            update.message.from.id + "\""
+                )
                 when {
                     !telegramParser!!.users.containsKey(update.message.chatId) ->
                         telegramParser!!.users[update.message.chatId] = User(update.message.chatId)
+
                     update.message.text.lowercase() == "ping" ->
                         sender.send("pong", update.message.chatId!!)
+
                     update.message.text.lowercase() == "pong" ->
                         sender.send("ping", update.message.chatId!!)
                 }
@@ -109,6 +123,7 @@ class TelegramBot : TelegramLongPollingBot(Config.token) {
                     }
                 }
             }
+
             else -> {}
         }
     }
