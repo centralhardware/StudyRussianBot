f182b5cdd729f77a92c804b5cf58d60b5f022dbc
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-05 19:54:08 +0200

    add health check

--- src/main/java/me/centralhardware/znatoki/studyRussianBot/Main.kt
+++ src/main/java/me/centralhardware/znatoki/studyRussianBot/Main.kt
@@ -1,12 +1,15 @@
 package me.centralhardware.znatoki.studyRussianBot
 
+import com.sun.net.httpserver.HttpServer
 import org.slf4j.LoggerFactory
 import me.centralhardware.znatoki.studyRussianBot.telegram.TelegramBot
+import java.net.InetSocketAddress
 
 /**
  * init telegram ApiContext and telegram bot, statistic
  */
 fun main() {
+    HttpServer.create().apply { bind(InetSocketAddress(80), 0); createContext("/health") { it.sendResponseHeaders(200, 0); it.responseBody.close() }; start() }
     WordManager.init()
     TelegramBot.init()
     LoggerFactory.getLogger("main").info("telegram bot run")
