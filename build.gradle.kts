c57b18f6257a8b046b1d26659355712695ecc303
Fedechkin Aleksei <alex@centralhardware.me>
2024-12-03 06:20:14 +0100

    bump ktgboapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.inmo:tgbotapi:20.0.1")
-    implementation("com.github.centralhardware:telegram-bot-commons:d96b131958")
+    implementation("dev.inmo:tgbotapi:21.0.0")
+    implementation("com.github.centralhardware:telegram-bot-commons:34c054a4a1")
     implementation("org.json:json:20240303")
     implementation("io.github.crackthecodeabhi:kreds:0.9.1")
     implementation("org.postgresql:postgresql:42.7.4")
