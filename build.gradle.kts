a4e478125c466ab9fbd131ebcf24ef52440ed38a
Fedechkin Aleksei <alex@centralhardware.me>
2025-02-21 17:47:29 +0100

    bump ktgboapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.inmo:tgbotapi:23.2.0")
-    implementation("com.github.centralhardware:ktgbotapi-commons:f0db12f60b")
+    implementation("dev.inmo:tgbotapi:24.0.0")
+    implementation("com.github.centralhardware:ktgbotapi-commons:6ef1dde4fe")
     implementation("org.json:json:20250107")
     implementation("io.github.crackthecodeabhi:kreds:0.9.1")
     implementation("org.postgresql:postgresql:42.7.5")
