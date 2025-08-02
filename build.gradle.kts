b97dfab364cdb249f23aed3a3505ed8757df9dd8
alex <alex@centralhardware.me>
2025-08-03 01:19:56 +0800

    bump commons

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     implementation("dev.inmo:tgbotapi:27.1.0")
-    implementation("com.github.centralhardware:ktgbotapi-commons:6ef1dde4fe")
+    implementation("com.github.centralhardware:ktgbotapi-commons:063599df7c")
     implementation("org.json:json:20250517")
     implementation("io.github.crackthecodeabhi:kreds:0.9.1")
     implementation("org.postgresql:postgresql:42.7.7")
