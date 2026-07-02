326c91dba22d91a113dd919ca1f246b5bb3acc9c
Alexey Fedechkin <alex@centralhardware.me>
2026-07-02 13:37:57 +0200

    Bump ktgbotapi-commons to d57cb77e (tgbotapi 35.1.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.centralhardware:ktgbotapi-commons:4db3d261")
+    implementation("com.github.centralhardware:ktgbotapi-commons:d57cb77e")
     implementation("org.json:json:20260522")
     implementation("io.github.crackthecodeabhi:kreds:0.9.1")
     implementation("org.postgresql:postgresql:42.7.12")
