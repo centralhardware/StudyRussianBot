d823a7ded30f8f2dd24a82196e38256ed26155c5
Alexey Fedechkin <alex@centralhardware.me>
2026-09-08 01:32:39 +0700

    Bump ktgbotapi-commons to e9eab760 (#86)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.centralhardware:ktgbotapi-commons:07ee206f")
+    implementation("com.github.centralhardware:ktgbotapi-commons:e9eab760")
     implementation("org.postgresql:postgresql:42.7.13")
     implementation("org.flywaydb:flyway-core:13.4.0")
     runtimeOnly("org.flywaydb:flyway-database-postgresql:13.4.0")
